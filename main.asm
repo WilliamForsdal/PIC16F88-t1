51e494299a9d025b7003e071baf6a3017d018d43
William Forsdal <william.forsdal@gmail.com>
2023-02-26 10:04:47 +0100

    uart tx works, needs cleanup

--- main.asm
+++ main.asm
@@ -42,46 +42,111 @@ main:
     ; BANKSEL     PORTA ; select bank of PORTA
     ; goto        main_loop
 
-    BANKSEL     OSCCON
-    MOVLW       0b01100000
-    MOVWF       OSCCON
+    call        sr_init_osc
+    call        sr_init_gpio
+    call        sr_init_uart
+    ; BANKSEL     OSCCON
+    ; MOVLW       0b01100000
+    ; MOVWF       OSCCON
 
-    BANKSEL     PORTA ; select bank of PORTA
-    CLRF        PORTA
-    BANKSEL     ANSEL 
-    MOVLW       0x00 ; Configure all pins
-    MOVWF       ANSEL ; as digital inputs
-    MOVLW       0x02
-    MOVWF       TRISA
+    ; BANKSEL     PORTA ; select bank of PORTA
+    ; CLRF        PORTA
+    ; BANKSEL     ANSEL 
+    ; MOVLW       0x00 ; Configure all pins
+    ; MOVWF       ANSEL ; as digital inputs
+    ; MOVLW       0x02
+    ; MOVWF       TRISA
     BANKSEL     PORTA ; select bank of PORTA
     goto        main_loop
 
 main_loop:
-    btfsc       PORTA, 1
-    bsf         PORTA, 0
-    bcf         PORTA, 0
+    ; btfsc       PORTA, 1
+    MOVLW       0x41
+    call        uart_tx
+    MOVLW       0x42
+    call        uart_tx
+    MOVLW       0x43
+    call        uart_tx
+    MOVLW       0x44
+    call        uart_tx
+    MOVLW       0x0D
+    call        uart_tx
+    MOVLW       0x0A
+    call        uart_tx
+    
+    BANKSEL     PORTA
+    MOVLW       200
+    MOVWF       0x21
+_main_loop_delay_1:
+    MOVWF       0x20
+_main_loop_delay_2:
+    DECFSZ      0x20, f
+    goto        _main_loop_delay_2
+    DECFSZ      0x21, f
+    goto        _main_loop_delay_1
+
     goto        main_loop
 
 
+uart_tx:
+    BANKSEL     PORTA
+    bsf         PORTA, 0
+    ; Start tx
+    BANKSEL     TXREG
+    MOVWF       TXREG ; starts tx
+    BANKSEL     TXSTA
+    NOP
+    NOP
+    NOP
 
+    ; Wait tx complete
+_uart_tx_wait:
+    btfss       TXSTA, 1
+    goto        _uart_tx_wait
 
-sr_osc_conf:
+    bcf         PORTA, 0
+    retlw       0
+
+sr_init_osc:
     BANKSEL     OSCCON
-    MOVLW       0b01100000
+    MOVLW       0b01110000
     MOVWF       OSCCON
     retlw       0
 
-
-
-sr_gpio_conf:
-    BANKSEL     PORTA ; select bank of PORTA
+sr_init_gpio:
+    
+    ; Clear outputs
+    BANKSEL     PORTA
     CLRF        PORTA
+
+    ; Configure all pins as digital inputs
     BANKSEL     ANSEL 
-    MOVLW       0x00 ; Configure all pins as digital inputs
+    MOVLW       0x00 
     MOVWF       ANSEL
-    MOVLW       0x01
+    MOVLW       0b00000010
     MOVWF       TRISA
     retlw       0
+
+
+sr_init_uart:
+    
+    ; Init SPBRG with baudrate
+    BANKSEL     SPBRG
+    MOVLW       0b10000000
+    MOVLW       SPBRG
+
+    ; Enable the asynchronous serial port by clearing bit SYNC and setting bit SPEN
+    BANKSEL     TXSTA
+    MOVLW       0b00100100
+    MOVWF       TXSTA
+    BANKSEL     RCSTA
+    MOVLW       0b10000000 ; bit7 = Enable Serial Port
+    MOVWF       RCSTA
+
+    retlw       0
+    
+
+    
 
 
 END reset_vector
